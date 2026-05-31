c4f3a3f896bb26396f5a89496a11da5813ef27a4
aredhering <aredhering@aol.com>
2026-05-31 01:42:39 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div style={{background:"red", color:"white", padding:"40px", fontSize:"24px"}}>
+      Hello! React is working!
+    </div>
+  );
+}
