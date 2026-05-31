0ba03dd51a7f48a278d66d4d962592b768efc23b
aredhering <aredhering@aol.com>
2026-05-31 01:46:25 -0500

    Delete src/App.jsx

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div style={{background:"red", color:"white", padding:"40px", fontSize:"24px"}}>
-      Hello! React is working!
-    </div>
-  );
-}
